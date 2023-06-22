d78c48fecf4dca89cf30fb7a38b579939dfc087e
yasmin.gil <yasmin.gil@meta.com.br>
2023-06-22 15:43:17 -0300

    criando testes unitarios consultaAssociadoPorId()

--- src/test/java/br/com/meta/apivotoscooperativa/service/AssociadoServiceTest.java
+++ src/test/java/br/com/meta/apivotoscooperativa/service/AssociadoServiceTest.java
@@ -3,20 +3,25 @@ package br.com.meta.apivotoscooperativa.service;
 import br.com.meta.apivotoscooperativa.dto.entrada.DadosCadastraAssociado;
 import br.com.meta.apivotoscooperativa.dto.saida.DadosRetornaAssociado;
 import br.com.meta.apivotoscooperativa.exception.AssociadoCpfInvalido;
+import br.com.meta.apivotoscooperativa.exception.AssociadoInexistenteException;
 import br.com.meta.apivotoscooperativa.exception.AssociadoJaExistenteException;
 import br.com.meta.apivotoscooperativa.model.Associado;
 import br.com.meta.apivotoscooperativa.repository.AssociadoRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -25,12 +30,12 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("associadoServiceTest")
-class AssociadoServiceTest {
+public class AssociadoServiceTest {
 
-    @MockBean
+    @Mock
     private AssociadoRepository associadoRepository;
 
-    @Autowired
+    @InjectMocks
     private AssociadoService associadoService;
 
 
@@ -55,7 +60,6 @@ class AssociadoServiceTest {
 
         Mockito.verify(associadoRepository, Mockito.times(1)).save(any(Associado.class));
 
-
     }
 
     @Test
@@ -72,11 +76,10 @@ class AssociadoServiceTest {
 
         Mockito.verify(associadoRepository, Mockito.never()).save(Mockito.any(Associado.class));
 
-
     }
 
     @Test
-    @DisplayName("deve receber um CPF unico e inválido e retornar a exceção AssociadoCpfInvalido" )
+    @DisplayName("deve receber um CPF unico e inválido e retornar a exceção AssociadoCpfInvalido")
     void cadastraAssociado_cenario3() {
         String cpfInvalido = "123.456.789-00";
 
@@ -89,8 +92,36 @@ class AssociadoServiceTest {
 
     }
 
+    @Test
+    @DisplayName("sucesso:Deve receber o associado com o ID fornecido existente no repositório e retornar um objeto DadosRetornaAssociadoEspecifico preenchido corretamente com os dados do associado.")
+    void consultaAssociadoPorId_Cenario1() {
+        Long id = 4L;
+        String cpf = "733.581.507-04";
 
+        when(associadoRepository.existsById(id)).thenReturn(true);
 
+        when(associadoRepository.getReferenceById(id))
+                .thenReturn(new Associado(id, cpf));
+
+        var dadosRetornaAssociadoEspecifico = associadoService.consultaAssociadoPorId(id);
+
+        assertEquals(id, dadosRetornaAssociadoEspecifico.id());
+        assertEquals(cpf, dadosRetornaAssociadoEspecifico.cpf());
+    }
+
+    @Test
+    @DisplayName("Deve receber o associado com o ID inválido ou inexistente e retornar uma exceção do tipo AssociadoInexistenteException")
+    void consultaAssociadoPorId_Cenario2() {
+
+        Long idInvalido = 100L;
+
+        when(associadoRepository.existsById(idInvalido)).thenReturn(false);
+
+        assertThrows(AssociadoInexistenteException.class, () -> {
+            associadoService.consultaAssociadoPorId(idInvalido);
+        });
+
+    }
 
 
 }
